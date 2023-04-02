fa6d33ef6b840b845a49a5be27f163ed9e6519ca
bairidilipkumar <bairidilipkumar@gmail.com>
2023-04-01 20:25:51 -0500

    Prototype
    Initial push on prototype!!!

---
+++ IS-Project/static/scripts/responses.js
@@ -0,0 +1,19 @@
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "rock") {
+        return "";
+    } else if (input == "paper") {
+        return "";
+    } else if (input == "") {
+        return "";
+    }
+
+    // Simple responses
+    if (input == "hello") {
+        return "";
+    } else if (input == "goodbye") {
+        return "";
+    } else {
+        return "";
+    }
+}
